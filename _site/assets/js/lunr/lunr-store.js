e4d79f289de29468f2da7e80b454c0c3afa3cd85
rubenlucas93 <ruben.lucas.zaragoza@hotmail.com>
2020-11-30 04:10:30 -0800

    updating blog

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Introduction (months 1 and 2)",
+        "excerpt":"Simple text to see the appearance and result of the documentation in a post.   Lectures           TFM Vanesa Fernández            [TFM Jéssica Fernández](https://gsyc.urjc.es/jmplaza/students/tfm-deeplearning_traffic_sensor-jessica-2020.pdf            TFM Francisco Pérez            TFM Nacho Arranz            TFM David Pascual            TFM Nacho Condés            TFM Nuria Oyaga            Detection Studio review       Lab work           Objects Detector            Detection Studio            Atom            Jekyll+GitPages      ","categories": ["your category"],
+        "tags": ["tag 1","tag 2","tag 3","tag 4"],
+        "url": "http://localhost:4000/your%20category/previous-work/",
+        "teaser":null}]
