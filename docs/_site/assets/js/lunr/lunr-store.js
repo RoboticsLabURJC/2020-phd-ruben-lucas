e011474faee1cd28150e8f4e9af01a4aaa8cf2c8
Doctorado <ruben.lucas.zaragoza@hotmail.com>
2021-01-08 08:55:32 -0800

    blog updated with month 4 work

--- docs/_site/assets/js/lunr/lunr-store.js
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -8,4 +8,9 @@ var store = [{
         "excerpt":"The goal for this month was to set the whole environment to work and start playing with reinforcement learning so the lectures can be consolidated with some exercises. Lectures Thesis of a DQN neural network able to beat an human gamer playing ATARI games Study of the effect of convolutional...","categories": ["your category"],
         "tags": ["tag 1","tag 2","tag 3","tag 4"],
         "url": "http://localhost:4000/your%20category/current-work/",
+        "teaser":null},{
+        "title": "QLearning (month 4)",
+        "excerpt":"The goal of this month is defining from scratch a model-free reinforcement learning algorithm and an environment to solve a typical path learning from source to goal problem. Additionally, some lectures have been useful to accomplish the month objectives. Lectures Reinforcement learning algorithms overview When to choose sarsa vs qlearning...","categories": ["your category"],
+        "tags": ["tag 1","tag 2","tag 3","tag 4"],
+        "url": "http://localhost:4000/your%20category/model_free_qlearning_algorithm/",
         "teaser":null}]
