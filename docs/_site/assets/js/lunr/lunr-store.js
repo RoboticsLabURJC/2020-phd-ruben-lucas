f384d09c467cfa72cd92bdd63e1390f993ad263c
Doctorado <>
2021-01-27 10:45:25 -0800

    updated layout to solve projects pages problem

--- docs/_site/assets/js/lunr/lunr-store.js
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -2,30 +2,30 @@ var store = [{
         "title": "Introduction (months 1 and 2)",
         "excerpt":"The goal this two first months is to land into machine learning and vision recognition world reading some previous master theses and its references to clarify some artificial intelligence basic concepts. Lectures TFM Vanesa Fernández TFM Jéssica Fernández TFM Francisco Pérez TFM Nacho Arranz TFM David Pascual TFM Nacho Condés...","categories": ["your category"],
         "tags": ["tag 1","tag 2","tag 3","tag 4"],
-        "url": "http://localhost:4000/your%20category/previous-work/",
+        "url": "http://localhost:4000/2020-phd-ruben-lucas/your%20category/previous-work/",
         "teaser":null},{
         "title": "Digging in (month 3)",
         "excerpt":"The goal for this month was to set the whole environment to work and start playing with reinforcement learning so the lectures can be consolidated with some exercises. Lectures Thesis of a DQN neural network able to beat an human gamer playing ATARI games Study of the effect of convolutional...","categories": ["your category"],
         "tags": ["tag 1","tag 2","tag 3","tag 4"],
-        "url": "http://localhost:4000/your%20category/current-work/",
+        "url": "http://localhost:4000/2020-phd-ruben-lucas/your%20category/current-work/",
         "teaser":null},{
         "title": "QLearning and Sarsa (month 4)",
         "excerpt":"The goal of this month is defining from scratch a model-free reinforcement learning algorithm and an environment to solve a typical path learning from source to goal problem. Additionally, some lectures have been useful to accomplish the month objective. Lectures Reinforcement learning algorithms overview When to choose sarsa vs qlearning...","categories": ["your category"],
         "tags": ["tag 1","tag 2","tag 3","tag 4"],
-        "url": "http://localhost:4000/your%20category/model_free_qlearning_algorithm/",
+        "url": "http://localhost:4000/2020-phd-ruben-lucas/your%20category/model_free_qlearning_algorithm/",
         "teaser":null},{
         "title": "QLearning (month 5 · Weeks 1 and 2)",
         "excerpt":"The goal of this two weeks is to solve the mountainCar problem using a reinforced learning algorithm with no neural network.  Besides that, this blog will be refined to better show the work progress and learnings.   Lab work      mountainCar openAIGym exercise with qlearning  ","categories": ["your category"],
         "tags": ["tag 1","tag 2","tag 3","tag 4"],
-        "url": "http://localhost:4000/your%20category/mountainCar_qlearning/",
+        "url": "http://localhost:4000/2020-phd-ruben-lucas/your%20category/mountainCar_qlearning/",
         "teaser":null},{
         "title": "Robot following path to goal with q learning and sarsa",
         "excerpt":"Reqs To execute this program you just need to install the following libraries: Python3 PyQt5 numpy Pandas matplotlib Manual The main goal of this kind of exercises is to learn how to develop a simple reinforced learning algorithm to make an agent learn the optimal path to the goal as...","categories": ["your category"],
         "tags": ["tag 1","tag 2","tag 3","tag 4"],
-        "url": "http://localhost:4000/projects/2021-01-08-model_free_qlearning_algorithm/",
+        "url": "http://localhost:4000/2020-phd-ruben-lucas/projects/2021-01-08-model_free_qlearning_algorithm/",
         "teaser":null},{
         "title": "MountainCar openAI Gym exercise solved using q learning",
         "excerpt":"Reqs To execute this program you just need to install the following libraries: Python3 PyQt5 numpy Pandas matplotlib Manual The main goal of this exercise is similar to the last proposed. To dig into the reinforcement learning basics. In this case, the exercise is not that simple, since some times...","categories": ["your category"],
         "tags": ["tag 1","tag 2","tag 3","tag 4"],
-        "url": "http://localhost:4000/projects/2021-01-22-mountainCar_qlearning/",
+        "url": "http://localhost:4000/2020-phd-ruben-lucas/projects/2021-01-22-mountainCar_qlearning/",
         "teaser":null}]
