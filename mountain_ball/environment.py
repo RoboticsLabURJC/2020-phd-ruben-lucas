273ab2e08738b698edf46d909223086f4dd82dfa
Doctorado <>
2021-01-31 16:25:19 -0800

    mountain_ball stil in phase beta

--- mountain_ball/environment.py
+++ mountain_ball/environment.py
@@ -6,10 +6,14 @@ import math
 
 import numpy as np
 
+import gym
+from gym import spaces
+from gym.utils import seeding
+
+from sympy import *
 
 
-
-class MountainCarEnv():
+class MountainBallEnv(gym.Env):
     """
     Description:
         The agent (a car) is started at the bottom of a valley. For any given
@@ -43,15 +47,31 @@ class MountainCarEnv():
          Episode length is greater than 200
     """
 
+    car_mass=4
+
+    # We have to create a "symbol" called x
+    x = Symbol('x')
+    #ramp
+    #heigh_function=0.3*x +2
+    #weird courves mountain
+    heigh_function=4*sin(0.3*x)**3+ 4.5
+    STEPS_PER_SECOND=100
+
+    metadata = {
+        'render.modes': ['human', 'rgb_array'],
+        'video.frames_per_second': 30
+    }
+
     def __init__(self, goal_velocity=0):
-        self.min_position = -1.2
-        self.max_position = 0.6
-        self.max_speed = 0.07
-        self.goal_position = 0.5
+        self.world_function= lambdify(self.x, self.heigh_function)
+        self.min_position = -11
+        self.max_position = 11
+        self.max_speed = 2
+        self.goal_position = 10
         self.goal_velocity = goal_velocity
 
-        self.force = 0.001
-        self.gravity = 0.0025
+        self.force = 0.1
+        self.gravity = 9.8
 
         self.low = np.array(
             [self.min_position, -self.max_speed], dtype=np.float32
@@ -62,30 +82,46 @@ class MountainCarEnv():
 
         self.viewer = None
 
-        self.action_space = [-1,0,1]
+        self.action_space = spaces.Discrete(3)
         self.observation_space = spaces.Box(
             self.low, self.high, dtype=np.float32
         )
 
-        #self.seed()
+        self.seed()
 
-    #def seed(self, seed=None):
-    #    self.np_random, seed = seeding.np_random(seed)
-    #    return [seed]
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
 
     def step(self, action):
+        #TODO hacerla genérica en función de la pendiente en la posición del mundo creado
         assert self.action_space.contains(action), "%r (%s) invalid" % (action, type(action))
+        f_prime = lambdify(self.x, self.heigh_function.diff(self.x))
 
         position, velocity = self.state
-        velocity += (action - 1) * self.force + math.cos(3 * position) * (-self.gravity)
+        #accelaration per second
+        acceleration=self.car_mass*self.gravity*math.sin(f_prime(position))
+        #acceleration per step
+        acceleration_step=acceleration/self.STEPS_PER_SECOND
+
+        #velocity per second
+        velocity+= ((action - 1) * self.force) - acceleration_step
+        #velocity per step
+        velocity_step=velocity/self.STEPS_PER_SECOND
+
+        position+=velocity_step
+
+        #velocity += (action - 1) * self.force + math.cos(3 * position) * (-self.gravity)
         velocity = np.clip(velocity, -self.max_speed, self.max_speed)
-        position += velocity
+        #position += velocity
         position = np.clip(position, self.min_position, self.max_position)
+
         if (position == self.min_position and velocity < 0):
             velocity = 0
 
         done = bool(
-            position >= self.goal_position and velocity >= self.goal_velocity
+            position >= self.goal_position
+            #position >= self.goal_position and velocity >= self.goal_velocity
         )
         reward = -1.0
 
@@ -93,14 +129,83 @@ class MountainCarEnv():
         return np.array(self.state), reward, done, {}
 
     def reset(self):
-        self.state = np.array([self.np_random.uniform(low=-0.6, high=-0.4), 0])
+        #TODO la posición inicial debe depender del rango de posiciones mostradas
+        #en la ventana y se debe configurar como constante
+        #self.state = np.array([self.np_random.uniform(low=-0.6, high=-0.4), 0])
+        self.state=[0.3,0]
         return np.array(self.state)
 
     def _height(self, xs):
-        return np.sin(3 * xs) * .45 + .55
+        return self.world_function(xs)
+
+    def render(self, mode='human'):
+        screen_width = 600
+        screen_height = 400
+
+        world_width = self.max_position - self.min_position
+        scale = screen_width / world_width
+        carwidth = 30
+        carheight = 15
+
+        if self.viewer is None:
+            from gym.envs.classic_control import rendering
+            self.viewer = rendering.Viewer(screen_width, screen_height)
+            xs = np.linspace(self.min_position, self.max_position, 100)
+            ys = self._height(xs)
+            xys = list(zip((xs - self.min_position) * scale, ys * scale))
+
+            self.track = rendering.make_polyline(xys)
+            self.track.set_linewidth(4)
+            self.viewer.add_geom(self.track)
+
+            clearance = 10
+
+            l, r, t, b = -carwidth / 2, carwidth / 2, carheight, 0
+            car = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
+            car.add_attr(rendering.Transform(translation=(0, clearance)))
+            self.cartrans = rendering.Transform()
+            car.add_attr(self.cartrans)
+            self.viewer.add_geom(car)
+            frontwheel = rendering.make_circle(carheight / 2.5)
+            frontwheel.set_color(.5, .5, .5)
+            frontwheel.add_attr(
+                rendering.Transform(translation=(carwidth / 4, clearance))
+            )
+            frontwheel.add_attr(self.cartrans)
+            self.viewer.add_geom(frontwheel)
+            backwheel = rendering.make_circle(carheight / 2.5)
+            backwheel.add_attr(
+                rendering.Transform(translation=(-carwidth / 4, clearance))
+            )
+            backwheel.add_attr(self.cartrans)
+            backwheel.set_color(.5, .5, .5)
+            self.viewer.add_geom(backwheel)
+            flagx = (self.goal_position-self.min_position) * scale
+            flagy1 = self._height(self.goal_position) * scale
+            flagy2 = flagy1 + 50
+            flagpole = rendering.Line((flagx, flagy1), (flagx, flagy2))
+            self.viewer.add_geom(flagpole)
+            flag = rendering.FilledPolygon(
+                [(flagx, flagy2), (flagx, flagy2 - 10), (flagx + 25, flagy2 - 5)]
+            )
+            flag.set_color(.8, .8, 0)
+            self.viewer.add_geom(flag)
+
+        pos = self.state[0]
+        self.cartrans.set_translation(
+            (pos-self.min_position) * scale, self._height(pos) * scale
+        )
+
+        f_prime = lambdify(self.x, self.heigh_function.diff(self.x))
+        self.cartrans.set_rotation(math.sin(f_prime(pos)))
+
+        return self.viewer.render(return_rgb_array=mode == 'rgb_array')
+
+    def get_keys_to_action(self):
+        # Control with left and right arrow keys.
+        return {(): 1, (276,): 0, (275,): 2, (275, 276): 1}
 
     def close(self):
         if self.viewer:
             self.viewer.close()
             self.viewer = None
-
