3fcb01ae53e0f89a9dc2d4fe3cd5e56644b07aea
Doctorado <>
2021-03-21 16:29:09 -0700

    updated post for this month work

--- docs/_site/assets/js/lunr/lunr-store.js
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -24,10 +24,15 @@ var store = [{
         "tags": ["tag 1","tag 2","tag 3","tag 4"],
         "url": "http://localhost:4000/2020-phd-ruben-lucas/your%20category/mountainBall_qlearning/",
         "teaser":null},{
-        "title": "projects standarization (month 6 · Weeks 1 and 2)",
-        "excerpt":"The goal of this two weeks is to have an homogeneus project structure and an easy to follow projects coding plan (unwritten guidelines) so all of the exercises are organized in the same way. Additionally, the introdction, QLearning and Sarsa sections of the sutton book have been reviewed besides the...","categories": ["your category"],
+        "title": "Projects standarization (month 6)",
+        "excerpt":"The goal of this month is to have an homogeneus project structure and an easy to follow projects coding plan (unwritten guidelines) so all of the exercises are organized in the same way. Additionally, the introdction, QLearning and Sarsa sections of the sutton book have been reviewed besides the other...","categories": ["your category"],
         "tags": ["tag 1","tag 2","tag 3","tag 4"],
         "url": "http://localhost:4000/2020-phd-ruben-lucas/your%20category/standarization/",
+        "teaser":null},{
+        "title": "Study of first section of sutton book (month 7)",
+        "excerpt":"The goal of this month is to fully read the first section of sutton book (8 chapters) and to fully understand the basics of reincorcement learning. Once we got enough background, it will be easier to follow the good practices and state-of-the-art of reinforcement learning. Additionally, since more algorithms will...","categories": ["your category"],
+        "tags": ["tag 1","tag 2","tag 3","tag 4"],
+        "url": "http://localhost:4000/2020-phd-ruben-lucas/your%20category/rl_basics_study/",
         "teaser":null},{
         "title": "Robot following path to goal with q learning and sarsa",
         "excerpt":"Reqs To execute this program you just need to install the following libraries: Python3 PyQt5 numpy Pandas matplotlib Manual The main goal of this kind of exercises is to learn how to develop a simple reinforced learning algorithm to make an agent learn the optimal path to the goal as...","categories": ["your category"],
@@ -43,4 +48,9 @@ var store = [{
         "excerpt":"Reqs To execute this program you just need to install the following libraries: Python3.7 PyQt5 numpy 1.16.5 Pandas matplotlib tensorflow 1.14.0 gym sympy stable-baselines Manual The main goal of this exercise is being able to configure owr own environment with its own physics. The biggest challenge here was to make...","categories": ["your category"],
         "tags": ["tag 1","tag 2","tag 3","tag 4"],
         "url": "http://localhost:4000/2020-phd-ruben-lucas/projects/2021-02-07-customized-mountainCar/",
+        "teaser":null},{
+        "title": "MountainCar openAI Gym exercise revisited after reading sutton section 1",
+        "excerpt":"TO BE COMPLETED ","categories": ["your category"],
+        "tags": ["tag 1","tag 2","tag 3","tag 4"],
+        "url": "http://localhost:4000/2020-phd-ruben-lucas/projects/2021-03-21-revisited_mountain_car/",
         "teaser":null}]
