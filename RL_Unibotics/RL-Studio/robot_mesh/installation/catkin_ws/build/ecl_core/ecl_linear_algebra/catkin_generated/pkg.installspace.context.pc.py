58424935fe0be653a72b7ad4c231e0dc548904fe
Doctorado <>
2021-07-26 15:00:28 -0700

    added RL-Studio projects

---
+++ RL_Unibotics/RL-Studio/robot_mesh/installation/catkin_ws/build/ecl_core/ecl_linear_algebra/catkin_generated/pkg.installspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "${prefix}/include;/home/rlucasz/Sophus;/usr/include/eigen3".split(';') if "${prefix}/include;/home/rlucasz/Sophus;/usr/include/eigen3" != "" else []
+PROJECT_CATKIN_DEPENDS = "ecl_build;ecl_eigen;ecl_exceptions;ecl_formatters;ecl_math".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lecl_linear_algebra;/home/rlucasz/Sophus/build/libSophus.so".split(';') if "-lecl_linear_algebra;/home/rlucasz/Sophus/build/libSophus.so" != "" else []
+PROJECT_NAME = "ecl_linear_algebra"
+PROJECT_SPACE_DIR = "/home/rlucasz/Desktop/doctorado/RL_Unibotics/RL-Studio/robot_mesh/installation/catkin_ws/install"
+PROJECT_VERSION = "0.62.3"
