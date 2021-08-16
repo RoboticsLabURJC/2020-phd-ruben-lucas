200fb662c1deb0d6366f136285c3af3a36c3a240
Doctorado <>
2021-08-16 11:39:58 -0700

    mountain_car problem updated

--- RL_Unibotics/RL-Studio/mountain_car/agents/envs/robot_mesh_position_env.py
+++ RL_Unibotics/RL-Studio/mountain_car/agents/envs/robot_mesh_position_env.py
@@ -6,8 +6,12 @@ from geometry_msgs.msg import Twist
 from gym import spaces
 from gym.utils import seeding
 # from sensor_msgs.msg import Image
+# import gazebo_msgs.msg
+# from gazebo_msgs.msg import geometry_msgs
 from gazebo_msgs.msg import ModelState
 from gazebo_msgs.srv import SetModelState, GetModelState
+from std_msgs.msg import Float64
+from gazebo_msgs.srv import SetPhysicsProperties
 
 # from agents.robot.settings import telemetry, x_row, center_image, width, height, telemetry_mask, max_distance
 from qlearn_template import MyEnv
@@ -40,7 +44,7 @@ class RobotMeshEnv(MyEnv):
         self.vel_pub.publish(vel_cmd)
 
         time.sleep(0.125)
-        self._gazebo_pause()
+        # self._gazebo_pause()
 
         # object_coordinates = self.model_coordinates("my_robot", "")
         # x_position = object_coordinates.pose.position.x
@@ -82,6 +86,27 @@ class RobotMeshEnv(MyEnv):
 
         self._gazebo_unpause()
 
+        #
+        # set_gravity = rospy.ServiceProxy('/gazebo/set_physics_properties', SetPhysicsProperties)
+        #
+        # time_step = Float64(0.001)
+        # max_update_rate = Float64(1000.0)
+        # gravity = geometry_msgs.msg.Vector3()
+        # gravity.x = 0.0
+        # gravity.y = 0.0
+        # gravity.z = 0.0
+        # ode_config = gazebo_msgs.msg.ODEPhysics()
+        # ode_config.auto_disable_bodies = False
+        # ode_config.sor_pgs_precon_iters = 0
+        # ode_config.sor_pgs_iters = 50
+        # ode_config.sor_pgs_w = 1.3
+        # ode_config.sor_pgs_rms_error_tol = 0.0
+        # ode_config.contact_surface_layer = 0.001
+        # ode_config.contact_max_correcting_vel = 0.0
+        # ode_config.cfm = 0.0
+        # ode_config.erp = 0.2
+        # ode_config.max_contacts = 20
+        # set_gravity(time_step.data, max_update_rate.data, gravity, ode_config)
 
         y, x, ori = self.get_position()
 
@@ -95,7 +120,7 @@ class RobotMeshEnv(MyEnv):
         state=(pos_x+pos_y)
 
 
-        self._gazebo_pause()
+        # self._gazebo_pause()
 
         return state
 
